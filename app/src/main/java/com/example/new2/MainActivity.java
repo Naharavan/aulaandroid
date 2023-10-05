dead06ab42846c3cb030d8f3f4ede8d7f9b19260
Naharavan <emailamigo@gmail.com>
2023-10-05 09:26:10 -0300

    fundo e logo

--- app/src/main/java/com/example/new2/MainActivity.java
+++ app/src/main/java/com/example/new2/MainActivity.java
@@ -10,8 +10,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView Resultado;
-    private TextView N1;
-    private TextView N2;
+    private TextView hN1;
+    private TextView mN2;
     private Button Calculo;
 
     @Override
@@ -19,17 +19,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Resultado = findViewById(R.id.Resultado);
-        N1 = findViewById(R.id.N1);
-        N2 = findViewById(R.id.N2);
-        Calculo = findViewById(R.id.Calc);
+        Resultado = findViewById(R.id.resultado);
+        hN1 = findViewById(R.id.homens);
+        mN2 = findViewById(R.id.mulheres);
+        Calculo = findViewById(R.id.calcular);
 
         Calculo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                double n1 = Integer.parseInt(String.valueOf(N1.getText()));
-                double n2 = Integer.parseInt(String.valueOf(N2.getText()));
+                double n1 = Integer.parseInt(String.valueOf(hN1.getText()));
+                double n2 = Integer.parseInt(String.valueOf(mN2.getText()));
                 double res = (n1 * 0.300) + (n2 * 0.250);
                 Resultado.setText("Resultado:" + res + "Kilos de carne");
 
